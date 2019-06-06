0116f65c225150173710003b6794e582f4d759d0
Lenka2023 <lenka20231@gmail.com>
2019-06-06 20:12:23 +0300

    make view by template

--- events/views.py
+++ events/views.py
@@ -2,26 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-   rem_data=''
-   if request.method=='POST':
+   context = {
+   'page_header': 'Contacts_form',
+   }
+   return render(request, 'default_app/contacts_form.html',context)
+   #rem='10'
+  #if request.method=='POST':
       # rem_data=request.POST   
-       st=request.POST.get('st')
-       et=request.POST.get('et')
-       rem=request.POST.get('rem') 
-         if et=st+10:
-             rem=''  
-    return HttpResponse("""<html> 
-   <body>       
-      <form action = "http://localhost:8000/events" method = "post"> 
-         <p>Enter Start_time:</p> 
-         <p><input type = "text" name = "st" /></p> 
-		 <p>Enter End_time:</p> 
-         <p><input type = "text" name = "et" /></p> 
-		 <p>Enter remember:</p> 
-		  <p><input type = "text" name = "rem" /></p>
-         <p><input type = "submit" value = "submit" /></p> 
-      </form>  
-      {{rem}}     
-   </body> 
-</html>""")
+       #st=request.POST.get('st')
+      # et=request.POST.get('et')
+       #rem=request.POST.get('rem') 
+         #if et=st+10:
+            # rem=''  
+
 	
--- firstcalendar/settings.py
+++ firstcalendar/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'firstcalendar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [[os.path.join(BASE_DIR, 'templates')],],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
