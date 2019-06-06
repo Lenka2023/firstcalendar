3347a0f14439461805109b046efa286190c674ec
Lenka2023 <lenka20231@gmail.com>
2019-06-06 13:15:35 +0300

    make show rem_data from form

--- events/views.py
+++ events/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+   rem_data=''
+   if request.method=='POST':
+       rem_data=request.POST   
+  # rem=request.POST.get('rem')   
     return HttpResponse("""<html> 
    <body>       
       <form action = "http://localhost:8000/events" method = "post"> 
-         <p>Enter time:</p> 
-         <p><input type = "text" name = "t" /></p> 
-		 <p>Enter time:</p> 
-         <p><input type = "text" name = "t" /></p> 
+         <p>Enter Start_time:</p> 
+         <p><input type = "text" name = "st" /></p> 
+		 <p>Enter End_time:</p> 
+         <p><input type = "text" name = "et" /></p> 
 		 <p>Enter remember:</p> 
 		  <p><input type = "text" name = "rem" /></p>
          <p><input type = "submit" value = "submit" /></p> 
-      </form>       
+      </form>  
+      {{rem_data}}     
    </body> 
 </html>""")
 	
