db4614ba1201acb7b8c1da1f1bc34fee5d15e8de
Lenka2023 <lenka20231@gmail.com>
2019-06-07 22:37:33 +0300

    send contacts_form_data to form

--- events/views.py
+++ events/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-   rem='10'
+   contacts_form_data='12'
+   if request.method=='POST':
+       rem_data=request.POST   
+       st=request.POST.get('st')
+       et=request.POST.get('et')
+       rem=request.POST.get('rem') 
+   #if et='st'+10:
+            # rem=''  
+
    context = {
    'page_header': 'Contacts_form',
+   'contacts_form_data':contacts_form_data,
    }
    return render(request, 'events/contacts_form.html', context)
 
